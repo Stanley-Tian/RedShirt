25f7aae85a99babe7646dddbba91f6dc5e07129a
史丹利复合田 <grasslight@qq.com>
2016-10-02 18:23:22 +0800

    first init 改将数据格式转为CoreData了

---
+++ RedShirt/Employee.swift
@@ -0,0 +1,21 @@
+//
+//  Employee.swift
+//  RedShirt
+//
+//  Created by 史丹利复合田 on 2016/10/2.
+//  Copyright © 2016年 史丹利复合田. All rights reserved.
+//
+
+import Foundation
+
+class Employee{
+    var name:String = ""
+    var portrait:String = ""
+    
+    init(name:String,portrait:String)
+    {
+        self.name = name
+        self.portrait = portrait
+
+    }
+}
---
+++ RedShirt/EmployeeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  EmployeeCollectionViewCell.swift
+//  RedShirt
+//
+//  Created by 史丹利复合田 on 2016/10/2.
+//  Copyright © 2016年 史丹利复合田. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var portraitImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+}
